61b9ebd62439dd7bf1595c93ac36c3364609de25
Guilherme Lima <Guilherme.Lima@ibm.com>
2024-03-05 13:23:21 -0300

    tests.test_store_sparql_descriptors: Update

--- tests/test_store_sparql_descriptors.py
+++ tests/test_store_sparql_descriptors.py
@@ -32,6 +32,26 @@ class TestStoreSPARQL_Descriptors(kif_WikidataSPARQL_StoreTestCase):
             wd.L(4471), Lexeme('x'),
             wd.mass, Property('x')]))
         self.assertEqual(len(ds), 6)
+        self.assertEqual(ds[0][0], wd.IBM)
+        self.assertEqual(ds[0][1].label, Text('IBM'))
+        self.assertIn(Text('I.B.M.'), ds[0][1].aliases)
+        self.assertEqual(ds[0][1].description, Text(
+            'American multinational technology corporation'))
+        self.assertEqual(ds[1][0], Item('x'))
+        self.assertIsNone(ds[1][1])
+        self.assertEqual(ds[2][0], wd.L(4471))
+        self.assertEqual(ds[2][1].lemma, Text('love'))
+        self.assertEqual(ds[2][1].category, wd.verb)
+        self.assertEqual(ds[2][1].language, wd.English)
+        self.assertEqual(ds[3][0], Lexeme('x'))
+        self.assertIsNone(ds[3][1])
+        self.assertEqual(ds[4][0], wd.mass)
+        self.assertEqual(ds[4][1].label, Text('mass'))
+        self.assertIn(Text('molecular weight'), ds[4][1].aliases)
+        self.assertEqual(ds[4][1].description, Text(
+            'mass (in colloquial usage also known as weight) of the item'))
+        self.assertEqual(ds[5][0], Property('x'))
+        self.assertIsNone(ds[5][1])
 
 
 if __name__ == '__main__':
