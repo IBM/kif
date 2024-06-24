09528d941974f415b22ee823bb52fd9ad31ba7c7
Guilherme Lima <Guilherme.Lima@ibm.com>
2024-06-24 15:52:16 -0300

    tests.model.value.test_property_template: Add new test

--- tests/model/value/test_property_template.py
+++ tests/model/value/test_property_template.py
@@ -1,17 +1,32 @@
 # Copyright (C) 2024 IBM Corp.
 # SPDX-License-Identifier: Apache-2.0
 
+import datetime
+
 from kif_lib import (
     Datatype,
+    Entity,
+    ExternalId,
     IRI,
     Item,
     KIF_Object,
     Lexeme,
     Property,
     PropertyTemplate,
+    Quantity,
+    Statement,
+    StatementTemplate,
+    String,
+    Text,
+    Time,
+    Value,
+    ValueSnak,
+    ValueSnakTemplate,
     Variable,
 )
-from kif_lib.typing import assert_type
+from kif_lib.itertools import product
+from kif_lib.model import TValue
+from kif_lib.typing import assert_type, cast, ClassVar
 
 from ...tests import kif_EntityTemplateTestCase
 
@@ -64,6 +79,89 @@ class Test(kif_EntityTemplateTestCase):
                 (PropertyTemplate(Variable('x'), Variable('y')),
                  {Variable('y', Datatype): Item('x')}),
             ])
+
+    _test__call__entities: ClassVar[list[Entity]] = [
+        Item('x'),
+        Lexeme('z'),
+        Property('y', Item.datatype)
+    ]
+
+    _test__call__values: ClassVar[list[TValue]] = [
+        'x',
+        0,
+        datetime.datetime(2024, 6, 24, tzinfo=datetime.timezone.utc),
+        ExternalId('x'),
+        IRI('x'),
+        Item('x'),
+        Property('x'),
+        Quantity(0),
+        String('x'),
+        Text('x', 'y'),
+        Time('2024-06-24')
+    ]
+
+    def test__call__(self) -> None:
+        # variant 1
+        self.assert_raises_bad_argument(
+            TypeError, 2, None,
+            'cannot coerce dict into Value',
+            (PropertyTemplate(Variable('x')), 'ValueSnakTemplate'), {})
+        self.assert_raises_bad_argument(
+            TypeError, 2, None,
+            'cannot coerce ValueSnak into Value',
+            (PropertyTemplate('x'), 'ValueSnak'),
+            ValueSnak(Property('p'), Item('x')))
+        # success
+        assert_type(
+            PropertyTemplate(Variable('x'))(String('x')), ValueSnakTemplate)
+        assert_type(
+            PropertyTemplate('x', Variable('y'))(String('x')),
+            ValueSnakTemplate)
+        for v in self._test__call__values:
+            self.assert_value_snak_template(
+                PropertyTemplate(Variable('x'))(v),
+                Property(Variable('x')), Value.check(v))
+            self.assert_value_snak(
+                cast(ValueSnak, PropertyTemplate('p')(v)),
+                Property('p'), Value.check(v))
+        # variant 2
+        self.assert_raises_bad_argument(
+            TypeError, 1, None,
+            'cannot coerce int into Entity',
+            (Property(Variable('x')), 'StatementTemplate'), 0, 'x')
+        self.assert_raises_bad_argument(
+            TypeError, 1, None,
+            'cannot coerce str into Entity',
+            (Property('x', Variable('y')), 'StatementTemplate'), 'x', 'x')
+        self.assert_raises_bad_argument(
+            TypeError, 1, None,
+            'cannot coerce IRI into Entity',
+            (PropertyTemplate(Variable('x')), 'StatementTemplate'),
+            IRI('x'), 'x')
+        self.assert_raises_bad_argument(
+            TypeError, 2, None,
+            'cannot coerce dict into Value',
+            (PropertyTemplate('x', Variable('y')),
+             'ValueSnakTemplate'), Item('x'), {})
+        # success
+        assert_type(
+            PropertyTemplate(Variable('p'))(Item('x'), IRI('y')),
+            StatementTemplate)
+        assert_type(
+            PropertyTemplate('x', Variable('y'))(Item('x'), IRI('y')),
+            StatementTemplate)
+        it = product(self._test__call__entities, self._test__call__values)
+        for e, v in it:
+            self.assert_statement_template(
+                PropertyTemplate(
+                    Variable('p'))(e, v), e,
+                ValueSnak(Property(Variable('p')), v))
+            self.assert_statement_template(
+                Property('p', Variable('q'))(e, v), e,
+                ValueSnak(Property('p', Variable('q')), v))
+            self.assert_statement(
+                cast(Statement, PropertyTemplate('p')(e, v)),
+                e, ValueSnak(Property('p'), v))
 
 
 if __name__ == '__main__':
