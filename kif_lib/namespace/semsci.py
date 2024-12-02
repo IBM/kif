47862714022756591f9b597b868852aec986bd03
Guilherme Lima <Guilherme.Lima@ibm.com>
2024-12-02 09:03:30 -0300

    kif_lib.namespace.semsci: Update CHEMINF namespace

--- kif_lib/namespace/semsci.py
+++ kif_lib/namespace/semsci.py
@@ -52,11 +52,17 @@ class CHEMINF:
     CAS_registry_number = SEMSCI.CHEMINF_000446
     ChEBI_identifier = SEMSCI.CHEMINF_000407
     ChEMBL_identifier = SEMSCI.CHEMINF_000412
+    covalent_unit_count_generated_by_pubchem_software_library = SEMSCI.CHEMINF_000369
+    defined_atom_stereocenter_count_generated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000370
+    defined_bond_stereocenter_count_generated_by_pubchem_software_library = SEMSCI.CHEMINF_000371
     drug_trade_name = SEMSCI.CHEMINF_000561
     exact_mass_calculated_by_pubchem_software_library = SEMSCI.CHEMINF_000338
     has_component = SEMSCI.CHEMINF_000478
     has_component_with_uncharged_counterpart = SEMSCI.CHEMINF_000480
     has_PubChem_normalized_counterpart = SEMSCI.CHEMINF_000477
+    heavy_atom_count_generated_by_pubchem_software_library = SEMSCI.CHEMINF_000373
+    hydrogen_bond_acceptor_count_calculated_by_cactvs = SEMSCI.CHEMINF_000388
+    hydrogen_bond_donor_count_calculated_by_cactvs = SEMSCI.CHEMINF_000387
     InChI_calculated_by_library_version_1_0_4 = SEMSCI.CHEMINF_000396
     InChIKey_generated_by_software_version_1_0_4 = SEMSCI.CHEMINF_000399
     is_stereoisomer_of = SEMSCI.CHEMINF_000461
@@ -65,10 +71,17 @@ class CHEMINF:
     IUPAC_Name_generated_by_LexiChem = SEMSCI.CHEMINF_000382
     molecular_formula_calculated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000335
     molecular_weight_calculated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000334
+    monoisotopic_mass_calculated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000337
     PubChem_compound_identifier_CID = SEMSCI.CHEMINF_000140
     pubchem_depositor_supplied_molecular_entity_name = SEMSCI.CHEMINF_000339
+    rotatable_bond_count_calculated_by_cactvs = SEMSCI.CHEMINF_000389
     similar_to_by_PubChem_2D_similarity_algorithm = SEMSCI.CHEMINF_000482
     structure_complexity_calculated_by_cactvs = SEMSCI.CHEMINF_000390
+    tautomer_count_calculated_by_cactvs = SEMSCI.CHEMINF_000391
+    total_formal_charge_calculated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000336
+    TPSA_calculated_by_cactvs = SEMSCI.CHEMINF_000392
+    undefined_atom_stereocenter_count_generated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000374
+    undefined_bond_stereocenter_count_generated_by_the_pubchem_software_library = SEMSCI.CHEMINF_000375
     xlogp3_calculated_by_the_xlogp3_software = SEMSCI.CHEMINF_000395
 
 
