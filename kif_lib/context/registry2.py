cd560ad2fc22222f56fdba4bef42909a9bedeccc
Guilherme Lima <Guilherme.Lima@ibm.com>
2025-02-18 13:48:00 -0300

    kif_lib.context: Add unregister() to registry2

--- kif_lib/context/registry2.py
+++ kif_lib/context/registry2.py
@@ -16,28 +16,35 @@ from ..model import (
     KIF_Object,
     Lexeme,
     Property,
+    String,
     TDatatype,
     Text,
     TItem,
     TProperty,
+    TString,
     TText,
 )
 from ..typing import (
     Any,
     Callable,
     cast,
+    Hashable,
     Iterable,
     Location,
+    Optional,
+    override,
     TypeAlias,
     TypeVar,
     Union,
 )
 from .context import Context
 
-T = TypeVar('T')
 E = TypeVar('E', bound=Union[Item, Property, Lexeme])
+T = TypeVar('T')
+TKey: TypeAlias = str
 TextMap: TypeAlias = dict[str, Text]
 TextSetMap: TypeAlias = dict[str, set[Text]]
+set_ = set
 
 
 class Registry(Cache):
@@ -69,6 +76,21 @@ class EntityRegistry(Registry):
 
     def __init__(self, context: Context) -> None:
         super().__init__(context)
+
+    @override
+    def get(self, obj: Hashable, key: str) -> Any:
+        assert isinstance(obj, Entity)
+        return super().get(obj.iri.content, key)
+
+    @override
+    def set(self, obj: Hashable, key: str, value: T) -> T:
+        assert isinstance(obj, Entity)
+        return super().set(obj.iri.content, key, value)
+
+    @override
+    def unset(self, obj: Hashable, key: str | None = None) -> Any:
+        assert isinstance(obj, Entity)
+        return super().unset(obj.iri.content, key)
 
     @overload
     def describe(
@@ -123,7 +145,7 @@ class EntityRegistry(Registry):
 
     def describe(
             self,
-            entity: E,
+            entity: Item | Property | Lexeme,
             function: Location | None = None
     ) -> Item.Descriptor | Property.Descriptor | Lexeme.Descriptor | None:
         function = function or self.describe
@@ -146,7 +168,7 @@ class EntityRegistry(Registry):
 
     @overload
     def register(self, entity: Item, **kwargs: Any) -> Item:
-        """Add or update item descriptor.
+        """Add or update entity data.
 
         Parameters:
            entity: Item.
@@ -165,7 +187,7 @@ class EntityRegistry(Registry):
 
     @overload
     def register(self, entity: Property, **kwargs: Any) -> Property:
-        """Add or update property descriptor.
+        """Add or update property data.
 
         Parameters:
            entity: Property.
@@ -186,10 +208,10 @@ class EntityRegistry(Registry):
 
     @overload
     def register(self, entity: Lexeme, **kwargs: Any) -> Lexeme:
-        """Add or update lemma descriptor.
+        """Add or update lexeme data.
 
         Parameters:
-           entity: Property.
+           entity: Lexeme.
            lemma: Lemma.
            category: Lexical category.
            language: Language.
@@ -275,6 +297,188 @@ class EntityRegistry(Registry):
             raise KIF_Object._should_not_get_here()
         return ret
 
+    @overload
+    def unregister(self, entity: Item, **kwargs: Any) -> bool:
+        """Remove item data.
+
+        Parameters:
+           entity: Item.
+           label: Label.
+           labels: Labels.
+           alias: Alias.
+           aliases: Aliases.
+           description: Description.
+           descriptions: Descriptions.
+           label_language: Language.
+           alias_language: Language.
+           description_language: Language.
+           all_labels: Whether to remove all labels.
+           all_aliases: Whether to remove all aliases.
+           all_descriptions: Whether to remove all descriptions.
+           all: Whether to remove all data.
+           function: Function or function name.
+
+        Returns:
+           ``True`` if successful; ``False`` otherwise.
+        """
+        ...                     # pragma: no cover
+
+    @overload
+    def unregister(self, entity: Property, **kwargs: Any) -> bool:
+        """Remove property data.
+
+        Parameters:
+           entity: Property.
+           label: Label.
+           labels: Labels.
+           alias: Alias.
+           aliases: Aliases.
+           description: Description.
+           descriptions: Descriptions.
+           label_language: Language.
+           alias_language: Language.
+           description_language: Language.
+           all_labels: Whether to remove all labels.
+           all_aliases: Whether to remove all aliases.
+           all_descriptions: Whether to remove all descriptions.
+           range: Whether to remove range.
+           inverse: Whether to remove inverse.
+           all: Whether to remove all data.
+           function: Function or function name.
+
+        Returns:
+           ``True`` if successful; ``False`` otherwise.
+        """
+        ...                     # pragma: no cover
+
+    @overload
+    def unregister(self, entity: Lexeme, **kwargs: Any) -> bool:
+        """Remove lexeme data.
+
+        Parameters:
+           entity: Lexeme.
+           lemma: Whether to remove lemma.
+           category: Whether to remove category.
+           language: Whether to remove language.
+           all: Whether to remove all data.
+           function: Function or function name.
+
+        Returns:
+           ``True`` if successful; ``False`` otherwise.
+        """
+        ...                     # pragma: no cover
+
+    def unregister(
+            self,
+            entity: Item | Property | Lexeme,
+            label: TText | None = None,
+            labels: Iterable[TText] = (),
+            alias: TText | None = None,
+            aliases: Iterable[TText] = (),
+            description: TText | None = None,
+            descriptions: Iterable[TText] = (),
+            label_language: TString | None = None,
+            alias_language: TString | None = None,
+            description_language: TString | None = None,
+            all_labels: bool = False,
+            all_aliases: bool = False,
+            all_descriptions: bool = False,
+            range: bool = False,
+            inverse: bool = False,
+            lemma: bool = False,
+            category: bool = False,
+            language: bool = False,
+            all: bool = False,
+            function: Location | None = None,
+            **kwargs: Any
+    ) -> bool:
+        function = function or self.unregister
+        return self._do_unregister(
+            entity,
+            labels=map(
+                lambda t: Text.check(t, function, 'labels'),
+                itertools.chain((label,), labels) if label else labels),
+            aliases=map(
+                lambda t: Text.check(t, function, 'aliases'),
+                itertools.chain((alias,), aliases) if alias else aliases),
+            descriptions=map(
+                lambda t: Text.check(t, function, 'descriptions'),
+                itertools.chain((description,), descriptions)
+                if description else descriptions),
+            label_language=String.check_optional(
+                label_language, None, function, 'label_language'),
+            alias_language=String.check_optional(
+                alias_language, None, function, 'alias_language'),
+            description_language=String.check_optional(
+                description_language, None, function, 'description_language'),
+            all_labels=bool(all_labels),
+            all_aliases=bool(all_aliases),
+            all_descriptions=bool(all_descriptions),
+            range=bool(range),
+            inverse=bool(inverse),
+            lemma=bool(lemma),
+            category=bool(category),
+            language=bool(language),
+            all=bool(all))
+
+    def _do_unregister(
+            self,
+            entity: Entity,
+            labels: Iterable[Text],
+            aliases: Iterable[Text],
+            descriptions: Iterable[Text],
+            label_language: String | None = None,
+            alias_language: String | None = None,
+            description_language: String | None = None,
+            all_labels: bool = False,
+            all_aliases: bool = False,
+            all_descriptions: bool = False,
+            range: bool = False,
+            inverse: bool = False,
+            lemma: bool = False,
+            category: bool = False,
+            language: bool = False,
+            all: bool = False
+    ) -> bool:
+        status: bool = False
+        if isinstance(entity, (Item, Property)):
+            for label in labels:
+                status |= bool(self._remove_label(entity, label))
+            for alias in aliases:
+                status |= bool(self._remove_alias(entity, alias))
+            for description in descriptions:
+                status |= bool(self._remove_description(entity, description))
+            if label_language:
+                status |= bool(self._remove_label(
+                    entity, language=label_language.content))
+            if alias_language:
+                status |= bool(self._remove_alias(
+                    entity, language=alias_language.content))
+            if description_language:
+                status |= bool(self._remove_description(
+                    entity, language=description_language.content))
+            if all_labels:
+                status |= bool(self._remove_all_labels(entity))
+            if all_aliases:
+                status |= bool(self._remove_all_aliases(entity))
+            if all_descriptions:
+                status |= bool(self._remove_all_descriptions(entity))
+            if isinstance(entity, Property):
+                if range:
+                    status |= bool(self._remove_range(entity))
+                if inverse:
+                    status |= bool(self._remove_inverse(entity))
+        elif isinstance(entity, Lexeme):
+            if lemma:
+                status |= bool(self._remove_lemma(entity))
+            if category:
+                status |= bool(self._remove_category(entity))
+            if language:
+                status |= bool(self._remove_language(entity))
+        if all:
+            status |= bool(self.unset(entity))
+        return status
+
     def _add_label(self, entity: Entity, label: Text) -> Text:
         return self._do_add_into_text_map(
             'labels', entity, label, self._do_add_into_text_map_tail)
@@ -289,6 +493,9 @@ class EntityRegistry(Registry):
             'labels', entity, label, language,
             self._do_remove_from_text_map_tail)
 
+    def _remove_all_labels(self, entity: Entity) -> TextMap | None:
+        return self.unset(entity, 'labels')
+
     def _add_alias(self, entity: Entity, alias: Text) -> Text:
         return self._do_add_into_text_map(
             'aliases', entity, alias, self._do_add_into_text_set_map_tail)
@@ -298,10 +505,13 @@ class EntityRegistry(Registry):
             entity: Entity,
             alias: Text | None = None,
             language: str | None = None
-    ) -> TextSetMap | set[Text] | None:
+    ) -> TextSetMap | set_[Text] | None:
         return self._do_remove_from_text_map(
             'aliases', entity, alias, language,
             self._do_remove_from_text_set_map_tail)
+
+    def _remove_all_aliases(self, entity: Entity) -> TextSetMap | None:
+        return self.unset(entity, 'aliases')
 
     def _add_description(self, entity: Entity, description: Text) -> Text:
         return self._do_add_into_text_map(
@@ -318,6 +528,9 @@ class EntityRegistry(Registry):
             'descriptions', entity, description, language,
             self._do_remove_from_text_map_tail)
 
+    def _remove_all_descriptions(self, entity: Entity) -> TextMap | None:
+        return self.unset(entity, 'labels')
+
     def _do_add_into_text_map(
             self,
             field: str,
@@ -325,8 +538,7 @@ class EntityRegistry(Registry):
             text: Text,
             tail: Callable[[T, Text], Text]
     ) -> Text:
-        k = entity.iri.content
-        t = cast(T, self.get(k, field) or self.set(k, field, {}))
+        t = cast(T, self.get(entity, field) or self.set(entity, field, {}))
         return tail(t, text)
 
     @staticmethod
@@ -350,16 +562,15 @@ class EntityRegistry(Registry):
             tail: Callable[[T, Text | None, str | None], Any]
     ) -> Any:
         assert text is None or language is None
-        k = entity.iri.content
-        t = cast(T | None, self.get(k, field))
+        t = cast(Optional[T], self.get(entity, field))
         if t is None:                           # nothing to do
             return None
         elif text is None and language is None:  # remove all
-            return cast(T, self.unset(k, field))
+            return cast(T, self.unset(entity, field))
         else:
             ret = tail(t, text, language)
             if not t:
-                self.unset(k, field)
+                self.unset(entity, field)
             return ret
 
     @staticmethod
@@ -385,60 +596,53 @@ class EntityRegistry(Registry):
             t: TextSetMap,
             text: Text | None,
             language: str | None
-    ) -> TextSetMap | set[Text] | Text | None:
-        if language is not None:  # remove by language
+    ) -> TextSetMap | set_[Text] | Text | None:
+        if language is not None and language in t:  # remove by language
             assert text is None
-            return cast(set[Text], t.pop(language))
-        elif text is not None:  # remove by content and language
+            return cast(set_[Text], t.pop(language))
+        elif text is not None and text.language in t:  # remove by text
             assert language is None
-            if text.language in t:
-                ts = t[text.language]
-                if text in ts:
-                    ts.remove(text)
-                    if not ts:
-                        del t[text.language]
-                    return text
-                else:
-                    return None
+            ts = t[text.language]
+            if text in ts:
+                ts.remove(text)
+                if not ts:
+                    del t[text.language]
+                return text
             else:
                 return None
         else:
-            raise KIF_Object._should_not_get_here()
+            return None
 
     def _add_range(self, property: Property, range: Datatype) -> Datatype:
-        return self.set(property.iri.content, 'range', range)
+        return self.set(property, 'range', range)
 
     def _remove_range(self, property: Property) -> Datatype | None:
-        return self.unset(property.iri.content, 'range')
+        return self.unset(property, 'range')
 
     def _add_inverse(
             self,
             property: Property,
             inverse: Property
     ) -> Property:
-        return self.set(property.iri.content, 'inverse', inverse)
+        return self.set(property, 'inverse', inverse)
 
-    def _remove_inverse(
-            self,
-            property: Property,
-            inverse: Property
-    ) -> Property | None:
-        return self.unset(property.iri.content, 'inverse')
+    def _remove_inverse(self, property: Property) -> Property | None:
+        return self.unset(property, 'inverse')
 
     def _add_lemma(self, lexeme: Lexeme, lemma: Text) -> Text:
-        return self.set(lexeme.iri.content, 'lemma', lemma)
+        return self.set(lexeme, 'lemma', lemma)
 
     def _remove_lemma(self, lexeme: Lexeme) -> Text | None:
-        return self.unset(lexeme.iri.content, 'lemma')
+        return self.unset(lexeme, 'lemma')
 
     def _add_category(self, lexeme: Lexeme, category: Item) -> Item | None:
-        return self.set(lexeme.iri.content, 'category', category)
+        return self.set(lexeme, 'category', category)
 
     def _remove_category(self, lexeme: Lexeme) -> Item | None:
-        return self.unset(lexeme.iri.content, 'category')
+        return self.unset(lexeme, 'category')
 
     def _add_language(self, lexeme: Lexeme, language: Item) -> Item | None:
-        return self.set(lexeme.iri.content, 'language', language)
+        return self.set(lexeme, 'language', language)
 
     def _remove_language(self, lexeme: Lexeme) -> Item | None:
-        return self.unset(lexeme.iri.content, 'language')
+        return self.unset(lexeme, 'language')
