c3c1b133e8965ddb4fb754b7de5db98f157d3aa5
Guilherme Lima <Guilherme.Lima@ibm.com>
2025-03-27 15:42:23 -0300

    kif_lib.store.sparql: Use WDQS as default IRI for WDQS_Store

--- kif_lib/store/sparql.py
+++ kif_lib/store/sparql.py
@@ -921,6 +921,10 @@ class WDQS_Store(
             **kwargs: Any
     ) -> None:
         assert store_name == self.store_name
+        if not args:
+            resolver_iri = self.context.options.vocabulary.wd.resolver
+            if resolver_iri is not None:
+                args = (resolver_iri,)
         if mapping is None:
             mapping = _SPARQL_Store._wikidata_mapping_constructor(
                 blazegraph=True,  # force
