b70e49051b994b408aa342d3f0b92fe311647049
Guilherme Lima <Guilherme.Lima@ibm.com>
2025-03-27 14:39:34 -0300

    tests.store.dbpedia_rdf: New tests

---
+++ tests/store/dbpedia_rdf/test_filter.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2025 IBM Corp.
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from kif_lib.vocabulary import db, wd
+
+from ...tests import StoreTestCase
+
+
+class Test(StoreTestCase):
+
+    @classmethod
+    def KB(cls):
+        return cls.S(
+            'dbpedia-rdf',
+            'tests/data/benzene-dbpedia.ttl',
+        )
+
+    def test_empty(self) -> None:
+        xf, F = self.xfilter_assertion(self.KB())
+        xf(F(snak_mask=F.SnakMask(0)), ())
+
+    def test_full(self) -> None:
+        xf, F = self.xfilter_assertion(self.KB())
+        xf(F(),
+           {wd.label(db.r('Benzene'), 'Benzene')})
+
+
+if __name__ == '__main__':
+    Test.main()
