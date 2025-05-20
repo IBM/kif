e74ee0c7ea750d426a4e04169b22925c55d8621f
Guilherme Lima <Guilherme.Lima@ibm.com>
2025-05-20 10:15:47 -0300

    tests.store: Add tests for CSV and JSON readers

---
+++ tests/store/json_reader/test_filter.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2025 IBM Corp.
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from kif_lib import Store
+
+from ...tests import StoreTestCase
+
+
+class Test(StoreTestCase):
+
+    def test(self) -> None:
+        kb = Store('json-reader', 'tests/data/benzene.json')
+        f, F = self.store_filter_assertion(kb)
+        f(F(), set(Store('rdf', 'tests/data/benzene.ttl').filter()))
+
+
+if __name__ == '__main__':
+    Test.main()
